bb888405c94c2598f1e22eeaf3d48353be6fc669
ethaneffendi <ethaneffendi@outlook.com>
2025-02-26 19:10:43 -0800

    26 February 2025, 7:10 P.M.

---
+++ (5) Map/Mandelbrot Set/Implementation/Mandelbrot Set Without Multiprocessing.py
@@ -0,0 +1,67 @@
+import cmath
+from PIL import Image
+import math
+import time
+
+# checks whehter or not the input c is a member of the Mandelbrot set (returns what color c should be in a Mandelbrot set visualization)
+def isInMandelbrotSet(c, maxIterations = 1000, maxModulus = 10 ** 6 + 1):
+    # seed is zero
+    x_0 = 0
+    # iteration counter
+    iterations = 0
+    # tracks what the last iteration produced
+    x_n = x_0
+    # tracks the modulus of the last x_n
+    modulus = abs(x_n)
+    # iteration
+    while iterations < maxIterations and modulus < maxModulus:
+        iterations += 1
+        # try to produce the next x_n, but if it is too big (OverflowError), break
+        try:
+            x_n = (x_n ** 2) + c
+            modulus = abs(x_n)
+        except OverflowError:
+            break
+    #test for divergence (c is a part of the Mandelbrot set, so it should graphically be colored black)
+    if iterations >= maxIterations:
+        return (0,0,0)
+    #test for divergence (c is not a part of the Mandelbrot set, so it should graphically be colored white)
+    elif abs(x_n) > maxModulus:
+        return (255, 255, 255)
+
+# receives a list of complex numbers and returns a list containing what color each complex number should be colored on a Mandelbrot set visualization
+def computeMandelbrotSet_without_multiprocessing(inputList, maxIterations = 1000, maxModulus = 10 ** 6 + 1):
+    # iterate through inputList
+    for i in range(len(inputList)):
+        # set inputList's value at i to be what color the complex number at i should be in a Mandelbrot set visualization
+        inputList[i] = isInMandelbrotSet(inputList[i], maxIterations, maxModulus)
+    return inputList
+
+# produces an x by y image of the Mandelbrot set using the colors specified in inputList
+def visualizeMandelbrotSet(inputList, x, y):
+    # make a new image using PIL.Image
+    im = Image.new(mode = 'RGB', size = (x, y))
+    # plot the pixel colors specified in inputList onto im
+    for i in range(x):
+        for j in range(y):
+            im.putpixel((i,j), inputList[j * x + i])
+    # show im
+    im.show()
+
+# produces the list of complex numbers to be tested when real values are tested from -2 to 0.5 and imaginary values are tested from -1.5 to 1.5 (for x by y image)
+def produceCList(x, y):
+    outputList = []
+    xIncrement = abs(-2 - 0.5) / x
+    yIncrement = abs(-1.5 - 1.5) / y
+    for j in range(y):
+        for i in range(x):
+            outputList.append(complex(-2 + i * xIncrement, 1.5 - j * yIncrement))
+    return outputList
+
+# putting it all together
+inputList = produceCList(250, 300)
+start_time = time.perf_counter()
+inputList = computeMandelbrotSet_without_multiprocessing(inputList)
+end_time = time.perf_counter()
+print(end_time - start_time)
+visualizeMandelbrotSet(inputList, 250, 300)
